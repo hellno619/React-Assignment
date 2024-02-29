d678d7992c6cb053c03bb4ea17ca16c25994386f
VISHNU <55990388+hellno619@users.noreply.github.com>
2024-02-29 15:58:20 +0530

    Add files via upload

---
+++ React-assignments/Day 1 Assignment/app.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dept from './Component/Doctor';
+import Student from './Component/Student';
+import Doctor from './Component/Doctor';
+import Demo from './Component/Demo';
+function App() {
+
+  //   // Scalar Variables
+  //   let uname = "Scott";
+  //   let age = 25;
+  //   let email = "scott@gmai.com";
+
+
+  //   // Array
+  //   let citiesArray = ["Hyderabad", "Mumbai", "Pune", "Chennai", "Goa", "Hyderabad", "Mumbai", "Pune", "Chennai", "Goa"];
+
+  //     Object
+
+  //    // Array of Objects
+
+
+
+
+  //   let resultArray = citiesArray.map((item) =>  
+  //   {
+  //     return <li>{item}</li>
+  //   });
+
+
+
+
+ 
+      {/* //       <h3>Data Handling in React Components</h3>     
+//       <hr/>
+
+//       <div>
+//         User Name :   {uname}  <br/>
+//         User Email :   {email}  <br/>
+//         User Age :   {age}  <br/>
+//       </div>
+//       <hr/>
+
+
+//       <ol>
+//         {resultArray}
+//       </ol>
+
+//       <hr/>
+
+//     <hr/> */}
+
+           {/* assignment1 */}
+return(
+  <>
+           {/* <Doctor/>
+           <Student/> */}
+           <Demo/>
+           </>
+           );
+      
+}
+
+
+export default App;
